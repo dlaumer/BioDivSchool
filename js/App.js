efdd5ec70a7f1016f770a96946e9d7762b13c27d
Daniel Laumer <daniel.laumer@gmail.com>
2023-08-03 06:05:31 -0600

    Small changes

--- js/App.js
+++ js/App.js
@@ -193,7 +193,7 @@ define([
             var queryParams = new URLSearchParams(window.location.search);
             queryParams.set("mode", "collection");
             queryParams.delete("admin");
-            let url = window.location.href.split("?")[0] + "?" + queryParams.toString();
+            let url = window.location.href.split("?")[0];
             domCtr.create("div", { className: "labelText", innerHTML: app.strings.get("link") + ": <a href=" + url + " target=_blank>" + url + "</a>", style: "width: 100%; margin-bottom: 10px;" }, document.getElementById("textInfo_" + app.strings.get("P06.title.1")));
             domCtr.create("div", { id: "qrcode" }, document.getElementById("textInfo_" + app.strings.get("P06.title.1")));
 
